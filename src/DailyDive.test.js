ed3ba7344969c07b2125bc8c3d1733e2fcce7df1
makadalman <marydalmlan@gmail.com>
2025-04-04 18:12:51 -0700

    Updates

--- src/DailyDive.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import DailyDive from './DailyDive';
-
-test('renders learn react link', () => {
-  render(<DailyDive />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
